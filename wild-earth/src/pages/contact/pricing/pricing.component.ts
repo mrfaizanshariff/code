ad45c14714d9a2d362f099fb459b0ea5dd95b43f
Mohammed Faizan Shariff <71890836+mrfaizanshariff@users.noreply.github.com>
2022-12-28 21:01:22 +0530

    Booking section Added

--- wild-earth/src/pages/contact/pricing/pricing.component.ts
+++ wild-earth/src/pages/contact/pricing/pricing.component.ts
@@ -9,6 +9,18 @@ export class PricingComponent implements OnInit {
   @Input() choice1:string='';
   @Input() choice2:string='';
   @Input() checkBoxNo:string='';
+  @Input() subChoice1:string='';
+  @Input() subChoice2:string='';
+  @Input() subChoice1Price:string='';
+  @Input() subChoice2Price:string='';
+  @Input() img1Center:string='';
+  @Input() img2Center:string='';
+  @Input() img1left:string='';
+  @Input() img2left:string='';
+  @Input() img2right:string='';
+  @Input() img1right:string='';
+  @Input() img1btm:string='';
+  @Input() img2btm:string='';
 
   constructor() { }
 
